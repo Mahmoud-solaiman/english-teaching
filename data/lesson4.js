ec77d33eead0cc0586d5e8f2424a7bc730404143
Mahmoud Solaiman <mahmoudalshahat420@gmail.com>
2025-07-20 15:13:40 +0300

    Adding lesson4

---
+++ data/lesson4.js
@@ -0,0 +1,112 @@
+const cards = [
+  {
+    front: {
+      image: '../images/lesson4/father.png',
+      audio: '../audios/front-audios/lesson4/myfatheris40yearsold.mp3',
+      sentence: "My father is 40 years old"
+    },
+    back: {
+      audio:'../audios/back-audios/lesson4/father.mp3',
+      answer:'Father'
+    }
+  }, 
+  {
+    front: {
+      image: '../images/lesson4/mother.png',
+      audio: '../audios/front-audios/lesson4/mymotherisahousewife.mp3',
+      sentence: "My mother is a housewife"
+    },
+    back: {
+      audio:'../audios/back-audios/lesson4/mother.mp3',
+      answer:"Mother"
+    }
+  },
+  {
+    front: {
+      image: '../images/lesson4/brother.png',
+      audio: '../audios/front-audios/lesson4/mybrotherisshort.mp3',
+      sentence: "My brother is short"
+    },
+    back: {
+      audio:'../audios/back-audios/lesson4/brother.mp3',
+      answer:"Brother"
+    }
+  },
+  {
+    front: {
+      image: '../images/lesson4/sister.png',
+      audio: '../audios/front-audios/lesson4/mysisteristall.mp3',
+      sentence: "My sister is tall"
+    },
+    back: {
+      audio:'../audios/back-audios/lesson4/sister.mp3',
+      answer:"Sister"
+    }
+  },
+  {
+    front: {
+      image: '../images/lesson4/sibling.png',
+      audio: '../audios/front-audios/lesson4/ihave4siblings.mp3',
+      sentence: "I have 4 siblings"
+    },
+    back: {
+      audio:'../audios/back-audios/lesson4/sibling.mp3',
+      answer:"Sibling"
+    }
+  },
+  {
+    front: {
+      image: '../images/lesson4/grandfather.png',
+      audio: '../audios/front-audios/lesson4/mygrandfatherisalwaysangry.mp3',
+      sentence: "My grandfather is always angry"
+    },
+    back: {
+      audio:'../audios/back-audios/lesson4/grandfather.mp3',
+      answer:"Grandfather"
+    }
+  },
+  {
+    front: {
+      image: '../images/lesson4/grandmother.png',
+      audio: '../audios/front-audios/lesson4/mygrandmotherisverynice.mp3',
+      sentence: "My grandmother is very nice"
+    },
+    back: {
+      audio:'../audios/back-audios/lesson4/grandmother.mp3',
+      answer:"Grandmother"
+    }
+  },
+  {
+    front: {
+      image: '../images/lesson4/nephew.png',
+      audio: '../audios/front-audios/lesson4/idonthavenephews.mp3',
+      sentence: "I don't have nephews"
+    },
+    back: {
+      audio:'../audios/back-audios/lesson4/nephew.mp3',
+      answer:"Nephew"
+    }
+  },
+  {
+    front: {
+      image: '../images/lesson4/niece.png',
+      audio: '../audios/front-audios/lesson4/iamhisniece.mp3',
+      sentence: "I'm his niece"
+    },
+    back: {
+      audio:'../audios/back-audios/lesson4/niece.mp3',
+      answer:"Niece"
+    }
+  },
+  {
+    front: {
+      image: '../images/lesson4/family.png',
+      audio: '../audios/front-audios/lesson4/ihaveabigfamily.mp3',
+      sentence: "I have a big family"
+    },
+    back: {
+      audio:'../audios/back-audios/lesson4/family.mp3',
+      answer:"family"
+    }
+  }
+];
